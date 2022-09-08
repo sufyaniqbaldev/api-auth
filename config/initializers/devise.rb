1d14552fdceafc4d670e421a7740aefc65b8bc51
Sufyan Iqbal <Sufyan.Iqbal@activekeysolutions.com>
2022-09-08 20:10:38 +0500

    new user creation sign in and out successfully add

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -263,7 +263,7 @@ Devise.setup do |config|
   # should add them to the navigational formats lists.
   #
   # The "*/*" below is required to match Internet Explorer requests.
-  config.navigational_formats = ['*/*', :html]
+  config.navigational_formats = []
 
   # The default HTTP method used to sign out a resource. Default is :delete.
   config.sign_out_via = :delete
@@ -310,8 +310,8 @@ Devise.setup do |config|
   # config.sign_in_after_change_password = true
   config.jwt do |jwt|
     jwt.secret = Rails.application.credentials.fetch(:secret_key_base)
-    jwt.dispatch_requests = ['POST', %r{^/users/sign_in$}]
-    jwt.revocation_requests = ['DELETE', %r{^/users/sign_out$}]
+    jwt.dispatch_requests = [['POST', %r{^/users/sign_in$}]]
+    jwt.revocation_requests = [['DELETE', %r{^/users/sign_out}]]
     jwt.expiration_time = 120.minutes.to_i
   end
 
